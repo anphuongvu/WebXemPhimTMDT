dae04e61e57eacdcca5046a20e662db2335fcc1b
ThaoUyen <thaouyen.letruong@gmail.com>
2022-11-01 22:00:41 +0700

    Them khach hang
    THem kháchhangf

---
+++ Nhom3_WebXemPhim/Areas/Admin/Controllers/KhachHangsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nhom3_WebXemPhim.Models;
+
+namespace Nhom3_WebXemPhim.Areas.Admin.Controllers
+{
+    public class KhachHangsController : Controller
+    {
+        private QLXemPhimEntities db = new QLXemPhimEntities();
+
+        // GET: Admin/KhachHangs
+        public ActionResult Index()
+        {
+            var khachHangs = db.KhachHangs.Include(k => k.TaiKhoan);
+            return View(khachHangs.ToList());
+        }
+
+        // GET: Admin/KhachHangs/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // GET: Admin/KhachHangs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaTaiKhoan = new SelectList(db.TaiKhoans, "MaTaiKhoan", "Username");
+            return View();
+        }
+
+        // POST: Admin/KhachHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKhachHang,MaTaiKhoan,TenKhachHang,Tuoi,email,sdt")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhachHangs.Add(khachHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaTaiKhoan = new SelectList(db.TaiKhoans, "MaTaiKhoan", "Username", khachHang.MaTaiKhoan);
+            return View(khachHang);
+        }
+
+        // GET: Admin/KhachHangs/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaTaiKhoan = new SelectList(db.TaiKhoans, "MaTaiKhoan", "Username", khachHang.MaTaiKhoan);
+            return View(khachHang);
+        }
+
+        // POST: Admin/KhachHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKhachHang,MaTaiKhoan,TenKhachHang,Tuoi,email,sdt")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khachHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaTaiKhoan = new SelectList(db.TaiKhoans, "MaTaiKhoan", "Username", khachHang.MaTaiKhoan);
+            return View(khachHang);
+        }
+
+        // GET: Admin/KhachHangs/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: Admin/KhachHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            db.KhachHangs.Remove(khachHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
