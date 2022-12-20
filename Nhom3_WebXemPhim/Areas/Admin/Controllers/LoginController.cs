b07b5d948e7dfe506f8872f1ada3462a8d77ebd6
anphuongvu <vuphuongan2012@gmail.com>
2022-12-21 02:52:50 +0700

    Đăng nhập admin

---
+++ Nhom3_WebXemPhim/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nhom3_WebXemPhim.Models;
+
+namespace Nhom3_WebXemPhim.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        QLXemPhimEntities db = new QLXemPhimEntities();
+        // GET: Admin/Login
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string sTaikhoan = f["txtTaiKhoan"].ToString();
+            string sMatkhau = f.Get("txtMatKhau").ToString();
+            var kh = db.TaiKhoans.SingleOrDefault(n => n.Username == sTaikhoan && n.Password == sMatkhau);
+
+            if (kh != null)
+            {
+                //Session["name"] = kh.TenKhachHang;
+                Session["Admin"] = kh;
+                return RedirectToAction("Index", "Home", new {area = "Admin"});
+            }
+            else
+            {
+                ViewBag.ThongBao = "Tên tài khoản hoặc mật khẩu không đúng";
+                return View();
+            }
+
+
+        }
+        public ActionResult DangXuat()
+        {
+            Session.Clear();
+            return RedirectToAction("DangNhap", "Login", new {area= "Admin"});
+        }
+    }
+}
